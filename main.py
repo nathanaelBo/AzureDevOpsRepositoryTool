344ebc88ee77985b253759c6933f850c69ba3b2a
Nathanael Bosilia <nathanael.bosilia@hotmail.de>
2021-02-20 20:53:54 +0100

    refactor: use argparse the right way and put functions into a class

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
 import argparse
 import azure_devops_connection
+import sys
 
 
-def main():
-    azure_devops_connection.init()
+class AzureDevOpsRepositoryTool():
+    def __init__(self):
+        azure_devops_connection.init()
 
-    parser = argparse.ArgumentParser(description='Helper Tool for Azure DevOps Repositories')
+        parser = argparse.ArgumentParser(
+            description='Helper Tool for Azure DevOps Repositories')
+        parser.add_argument('command', help='Subcommand to run')
+        args = parser.parse_args(sys.argv[1:2])
 
-    parser.add_argument('-o', '--open', type=str, required=False, dest='open_repository', action='store',
-                        choices=azure_devops_connection.REPOSITORIES, help='Opens an Azure DevOps Repository in your default browser')
-    parser.add_argument('-i', '--info', type=str, required=False,
-                        dest='get_repository_info', action='store', choices=azure_devops_connection.REPOSITORIES,
-                        help='Shows information about an Azure DevOps Repository')
-    parser.add_argument('-c', '--create', type=str, required=False,
-                        dest='create_repository', action='store',
-                        help='Creates a new Azure DevOps Repository')
-    parser.add_argument('-l', '--list', required=False,
-                        dest='show_repository_list', action='store_true',
-                        help='Lists all existing Azure DevOps Repositories')
+        if not hasattr(self, args.command):
+            print('Unrecognized command')
+            parser.print_help()
+            exit(1)
 
-    args = parser.parse_args()
+        getattr(self, args.command)()
 
-    if args.open_repository:
-        azure_devops_connection.open_repository(args.open_repository)
-    elif args.get_repository_info:
-        azure_devops_connection.get_repository_info(args.get_repository_info)
-    elif args.create_repository:
-        azure_devops_connection.create_repository(args.create_repository)
-    elif args.show_repository_list:
+    def open(self):
+        parser = argparse.ArgumentParser(
+            description='Opens an Azure DevOps Repository in your default browser')
+        parser.add_argument(
+            'repository', choices=azure_devops_connection.REPOSITORIES)
+        args = parser.parse_args(sys.argv[2:])
+
+        azure_devops_connection.open_repository(args.repository)
+
+    def info(self):
+        parser = argparse.ArgumentParser(
+            description='Shows information about an Azure DevOps Repository')
+        parser.add_argument(
+            'repository', choices=azure_devops_connection.REPOSITORIES)
+        args = parser.parse_args(sys.argv[2:])
+
+        azure_devops_connection.get_repository_info(args.repository)
+
+    def create(self):
+        parser = argparse.ArgumentParser(
+            description='Creates a new Azure DevOps Repository')
+        parser.add_argument('repository')
+        args = parser.parse_args(sys.argv[2:])
+
+        azure_devops_connection.create_repository(args.repository)
+
+    def list(self):
+        parser = argparse.ArgumentParser(
+            description='Lists all existing Azure DevOps Repositories')
         azure_devops_connection.print_repository_list()
-    else:
-        return
 
 
 if __name__ == '__main__':
-    main()
+    AzureDevOpsRepositoryTool()
